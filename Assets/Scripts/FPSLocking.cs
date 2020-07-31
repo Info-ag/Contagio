3bdefdc00a34b6fd8e16f56e8a40cc65d1cb8e56
stone3311 <fenstein12@googlemail.com>
2020-07-31 22:21:37 +0200

    Added FPS limiter
    Limit game to 60 FPS to avoid wasting energy on mobile devices

---
+++ Assets/Scripts/FPSLocking.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLocking : MonoBehaviour
+{
+    public int targetFPS = 60;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Application.targetFrameRate = 60;
+    }
+}
